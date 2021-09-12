361f038330c3a4765ef7a01913fb4f0b1c564896
Vít Šalomon <vit@salomon.cloud>
2021-09-12 14:39:18 +0200

    feat: TSfied

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+// import reportWebVitals from './reportWebVitals';
+
+render(
+  <StrictMode>
+    Hello
+  </StrictMode>,
+  document.getElementById('root')
+);
+
+// reportWebVitals(console.log);
