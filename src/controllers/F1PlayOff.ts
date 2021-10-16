de6b46cd221c022dbb9a69ce2e9d15a527fa7bfe
Vít Šalomon <vit@salomon.cloud>
2021-10-16 13:12:04 +0200

    chore: minimal code for playoff rules to compile

--- src/controllers/F1PlayOff.ts
+++ src/controllers/F1PlayOff.ts
@@ -1,5 +1,34 @@
+import { loadSeason, PATHS, Season } from '../data';
 import { SeasonController } from './interface';
 
 export class F1PlayOff implements SeasonController {
+    season: Season | null = null;
 
+    setters = {
+        setRaceIndex: (_: number | ((_: number) => number)) => {},
+    };
+
+    async init(season: keyof typeof PATHS) {
+        this.season = await loadSeason(season);
+    }
+
+    destruct() {
+
+    }
+
+    isPlayOffRunning() {
+        return false;
+    }
+
+    getRegularDrivers() {
+        return this.season!.drivers;
+    }
+
+    getPlayOffDrivers() {
+        return this.season!.drivers;
+    }
+
+    getRace() {
+        return this.season!.races[0];
+    }
 }
